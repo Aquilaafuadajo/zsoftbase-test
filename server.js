7f1ee74eb9dac780f304600cb95bfca56fb2867e
Emmanuel Afuadajo <aquilaafuadajo@gmail.com>
2020-12-05 02:25:04 +0100

    updated package.json

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config({path: './config.env'})
 
 const app = require('./app')
-const DB = process.env.DATABASE_LOCAL
+const DB = process.env.DATABASE
 
 mongoose.connect(DB, {
   useNewUrlParser: true,
